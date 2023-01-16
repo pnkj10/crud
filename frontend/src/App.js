3200f75e6908c0d5d437451207359d0d4c7e2c33
_pnkj10 <pankajlokhande1981999@gmail.com>
2023-01-16 22:47:41 +0530

    a

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import DisplayUsers from './Components/DisplayUsers';
+import DeleteUser from './Components/DeleteUsers';
+import InsertUser from './Components/InsertUser';
+import UpdateUser from './Components/UpdateUser';
+
+function App() {
+  return (
+   <>
+   <InsertUser/>
+   <DeleteUser/>
+   <UpdateUser/>
+   <DisplayUsers/>
+   </>
+  );
+}
+
+export default App;
